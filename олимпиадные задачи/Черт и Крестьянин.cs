825551d6fbcf87e8ebd796912d5a33aed94d840d
Stefan <144349375+StefanWoolf@users.noreply.github.com>
2023-12-20 15:46:42 +0600

    Add files via upload

---
+++ олимпиадные задачи/Черт и Крестьянин.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Andrey_And_Yasha
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите MaxN:");
+        int MaxN = int.Parse(Console.ReadLine());
+        int answer = MaxN;
+
+        for (int z = 2; z <= Math.Ceiling(Math.Log(MaxN, 2)) + 1; z++)
+        {
+            int n = (int)Math.Pow(2, z) - 1;
+            int delitel = MaxN / n;
+            answer += delitel;
+        }
+
+        Console.WriteLine(answer);
+    }
+}
