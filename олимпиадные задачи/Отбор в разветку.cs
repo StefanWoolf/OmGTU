dd390acacd1b25883be8146b4da375bef136795d
Stefan <144349375+StefanWoolf@users.noreply.github.com>
2023-12-20 15:56:24 +0600

    Add files via upload

---
+++ олимпиадные задачи/Отбор в разветку.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число:");
+        int lot = int.Parse(Console.ReadLine());
+        int result = Recursion(lot);
+        Console.WriteLine(result);
+    }
+
+    static int Recursion(int lot)
+    {
+        if (lot == 3)
+        {
+            return 1;
+        }
+        else if (lot < 3 || lot == 4)
+        {
+            return 0;
+        }
+        return Recursion(lot / 2) + Recursion((int)Math.Ceiling((double)lot / 2));
+    }
+}
